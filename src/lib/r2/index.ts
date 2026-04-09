6f0947fa03ba0a086a5dfc17512e4021b4a5c99b
mlhkhariom <mlhkhariom@gmail.com>
2026-04-09 07:29:43 +0530

    fix: correct account ID to ec0a9ef71c2605e8e98f56312162e5ee (R2 is separate)

--- src/lib/r2/index.ts
+++ src/lib/r2/index.ts
@@ -5,7 +5,7 @@ export async function uploadToR2(
   contentType: string
 ): Promise<string> {
   await r2.put(key, file, { httpMetadata: { contentType } });
-  return `https://f53940e94f9aadb31afc5d7019f82096.r2.cloudflarestorage.com/mlhk-in/${key}`;
+  return `https://ec0a9ef71c2605e8e98f56312162e5ee.r2.cloudflarestorage.com/mlhk-in/${key}`;
 }
 
 export async function deleteFromR2(r2: R2Bucket, key: string) {
